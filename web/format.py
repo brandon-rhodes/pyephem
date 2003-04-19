41eef49cda4d56237f5ab8f7119d5ddea25aabb8
Brandon Craig Rhodes <brandon@rhodesmill.org>
2003-04-19 23:47:31 +0000

    Added a table of contents to the manual.

--- web/format.py
+++ web/format.py
@@ -22,4 +22,33 @@ content = re.compile(r' *(>>>|\.\.\.) ([^#\n]*)'
 for rx, sb in substitutions:
     content = re.sub(rx, sb, content)
 
+def heading_to_name(s):
+    return ''.join(s.split())
+
+heading_re = re.compile(r'<h([2-9])>(.*?)</h\1>')
+headings = heading_re.findall(content)
+
+toc = '<dl>'
+last = 0
+for levelstr, heading in headings:
+    level = int(levelstr)
+    slink = '<a href="#%s">%s</a>' % (heading_to_name(heading), heading)
+    if level == 2:
+        toc += '<dt>%s\n' % slink
+    elif level == 3:
+        if last != 3:
+            toc += '<dd>'
+        toc += '%s<br>\n' % slink
+    last = level
+toc += '</dl>'
+
+content = content.replace('_TABLE_OF_CONTENTS_', toc)
+
+def named_heading(match):
+    level, heading = match.groups()
+    name = heading_to_name(heading)
+    return '<h%s><a name="%s">%s</a></h%s>' % (level, name, heading, level)
+
+content = heading_re.sub(named_heading, content)
+
 sys.stdout.write(content)
