74bd9096c9f9f19660214820aae5b77c57ed6662
Brandon Craig Rhodes <brandon@rhodesmill.org>
2004-09-04 15:13:10 +0000

    Simplified MANIFEST.in and setup.py by using filename wildcards.

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from distutils.core import setup, Extension
 from glob import glob
 
+# The `pyephem' module is built from every .c file in the libastro
+# directory plus ...
+
 libastro_version = '3.6.1'
 libastro_files = glob('libastro-%s/*.c' % libastro_version)
 
+ext_modules = [
+    Extension('ephem', ['src/ephem.c'] + libastro_files,
+              include_dirs=['libastro-' + libastro_version]),
+              #extra_compile_args = ["-O2", "-ffast-math"],
+    ]
+
 setup(name = 'pyephem',
       version = '3.6.1a',
-      description = 'Python interface to the XEphem libastro library',
+      description = 'computational astronomy routines from XEphem',
       author = 'Brandon Craig Rhodes',
       author_email = 'brandon@rhodesmill.org',
       url = 'http://www.rhodesmill.org/brandon/projects/pyephem.html',
-      ext_modules = [Extension('ephem', ['src/ephem.c'] + libastro_files,
-                               #extra_compile_args = ["-O2", "-ffast-math"],
-                               include_dirs=['libastro-' + libastro_version])])
+      ext_modules = ext_modules)
