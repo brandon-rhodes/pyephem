a9d13742cb0f7d43cf376ac165dfc4b5dc8fe641
Brandon Craig Rhodes <brandon@rhodesmill.org>
2004-10-24 04:54:54 +0000

    Removed clunky now_init function, moved its code inline.

--- ephem.c
+++ ephem.c
@@ -626,14 +626,6 @@ static int set_f_pa(PyObject *self, PyObject *value, void *v)
  * Constructor and methods.
  */
 
-static void now_init(Now *now)
-{
-     now->n_lat = now->n_lng = now->n_elev = 0;
-     now->n_temp = 15.0;
-     now->n_dip = 0;
-     now->n_tz = 0;
-}
-
 static int Observer_init(PyObject *self, PyObject *args, PyObject *kwds)
 {
      Observer *o = (Observer*) self;
@@ -641,9 +633,11 @@ static int Observer_init(PyObject *self, PyObject *args, PyObject *kwds)
      if (!PyArg_ParseTupleAndKeywords(args, kwds, ":Observer", kwlist))
 	  return -1;
      o->now.n_mjd = mjd_now();
-     o->now.n_epoch = J2000;
+     o->now.n_lat = o->now.n_lng = o->now.n_tz = o->now.n_elev
+	  = o->now.n_dip = 0;
+     o->now.n_temp = 15.0;
      o->now.n_pressure = 1010;
-     now_init(&o->now);
+     o->now.n_epoch = J2000;
      return 0;
 }
 
@@ -945,9 +939,11 @@ static PyObject* Body_compute(PyObject *self, PyObject *args, PyObject *kwds)
 	  /* Since libastro always does topocentric computation, we
 	     need to provide a reasonable location and weather. */
 	  body->now.n_mjd = when_mjd;
-	  body->now.n_epoch = epoch_mjd;
-	  now_init(&body->now);
+	  body->now.n_lat = body->now.n_lng = body->now.n_tz = body->now.n_elev
+	       = body->now.n_dip = 0;
+	  body->now.n_temp = 15.0;
 	  body->now.n_pressure = 0; /* no refraction */
+	  body->now.n_epoch = epoch_mjd;
 
 	  body->obj.o_flags = VALID_GEO;
      }
