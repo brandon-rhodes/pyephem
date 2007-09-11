6a5427a3f009f12b8f7f0600d8e74fb9c4942fe9
Brandon Craig Rhodes <brandon@rhodesmill.org>
2007-09-11 21:26:33 +0000

    Renamed the "_libastro" module in the package init file back to normal.

--- src/__init__.py
+++ src/__init__.py
@@ -1,40 +1,53 @@
-# The core functionalty of PyEphem lives in the C-language _ephem
-# module, which packages the "libastro" astronomy routines as
+# The core functionalty of PyEphem lives in the C-language _libastro
+# module, which packages the astronomy routines from XEphem as
 # convenient Python types.
 
-import ephem._libastro as _
+import ephem._libastro as _libastro
 
-Observer = _.Observer
-degrees = _.degrees
-date = _.date
-hours = _.hours
-minute = _.minute
-second = _.second
+# We make available several basic types from _libastro.
 
-Body = _.Body
-Planet = _.Planet
-PlanetMoon = _.PlanetMoon
-FixedBody = _.FixedBody
-EllipticalBody = _.EllipticalBody
-ParabolicBody = _.ParabolicBody
-HyperbolicBody = _.HyperbolicBody
-EarthSatellite = _.EarthSatellite
+Observer = _libastro.Observer
+degrees = _libastro.degrees
+date = _libastro.date
+hours = _libastro.hours
+minute = _libastro.minute
+second = _libastro.second
 
-readdb = _.readdb
-readtle = _.readtle
-constellation = _.constellation
-separation = _.separation
-now = _.now
+Body = _libastro.Body
+Planet = _libastro.Planet
+PlanetMoon = _libastro.PlanetMoon
+FixedBody = _libastro.FixedBody
+EllipticalBody = _libastro.EllipticalBody
+ParabolicBody = _libastro.ParabolicBody
+HyperbolicBody = _libastro.HyperbolicBody
+EarthSatellite = _libastro.EarthSatellite
 
-#
+readdb = _libastro.readdb
+readtle = _libastro.readtle
+constellation = _libastro.constellation
+separation = _libastro.separation
+now = _libastro.now
 
-for index, classname, name in _.builtin_planets():
+# We also create a Python class ("Mercury", "Venus", etcetera) for
+# each planet and moon for which _libastro offers specific algorithms.
+
+for index, classname, name in _libastro.builtin_planets():
     exec '''
-class %s(_.%s):
+class %s(_libastro.%s):
     __planet__ = %r
 ''' % (name, classname, index)
 
-#
+del index, classname, name
 
-Saturn = _.Saturn
-Moon = _.Moon
+# We now replace two of the classes we have just created, because
+# _libastro actually provides separate types for two of the bodies.
+
+Saturn = _libastro.Saturn
+Moon = _libastro.Moon
+
+# THINGS TO DO:
+# Better docs
+# Easy date conversions (to and from datetime)
+# Newton solver
+# Use newton to provide risings and settings
+# Use newton to find equinoxes/solstices
