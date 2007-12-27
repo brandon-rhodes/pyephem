8878f6b0ee61b93ca8a0a7f2a6b4d27d08ba68fe
Brandon Craig Rhodes <brandon@rhodesmill.org>
2007-12-27 03:13:00 +0000

    Added classifiers and improved PyEphem project description.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ except:
     from distutils.core import setup, Extension
 from glob import glob
 
+__version__ = '3.7.2.2'
+
 # The `pyephem' module is built from every .c file in the libastro
 # directory plus ...
 
@@ -11,12 +13,20 @@ libastro_version = '3.7.2'
 libastro_files = glob('libastro-%s/*.c' % libastro_version)
 
 setup(name = 'pyephem',
-      version = '3.7.2.2',
-      description = 'computational astronomy routines for Python',
+      version = __version__,
+      description = 'Scientific-grade astronomy routines for computing planet positions,'
+      ' rising and setting times, equinoxes, moon phases, and more',
       license = 'LGPL',
       author = 'Brandon Craig Rhodes',
       author_email = 'brandon@rhodesmill.org',
       url = 'http://rhodesmill.org/pyephem/',
+      classifiers = [
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved ::'
+        ' GNU Library or Lesser General Public License (LGPL)',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        ],
       packages = [ 'ephem', 'ephem.tests' ],
       package_dir = { '': 'src' },
       package_data = { 'ephem': ['doc/*.rst', 'tests/usno/*.txt'],},
