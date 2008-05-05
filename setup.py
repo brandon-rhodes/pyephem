23df0d6cf2b1d1b3bf1b61210f567d3c218ffe47
Brandon Craig Rhodes <brandon@rhodesmill.org>
2008-05-05 04:11:20 +0000

    Explained to the "setup.py" script that binary moon data should now be included with libastro (making it much larger, it happens).

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ __version__ = '3.7.2.3'
 
 libastro_version = '3.7.2'
 libastro_files = glob('libastro-%s/*.c' % libastro_version)
+libastro_data = glob('extensions/data/*.c')
 
 def read(*filenames):
     return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
@@ -37,7 +38,8 @@ setup(name = 'pyephem',
       package_data = { 'ephem': ['doc/*.rst', 'tests/usno/*.txt'],},
       test_suite = 'ephem.tests',
       ext_modules = [
-    Extension('ephem._libastro', ['extensions/_libastro.c'] + libastro_files,
+    Extension('ephem._libastro',
+              ['extensions/_libastro.c'] + libastro_files + libastro_data,
               include_dirs=['libastro-' + libastro_version],
               )],
       )
