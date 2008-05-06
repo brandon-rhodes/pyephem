1b4fd9b7c3668c54ca6a18cf7f5f49f55c9f48ae
Brandon Craig Rhodes <brandon@rhodesmill.org>
2008-05-06 11:38:37 +0000

    Brought Mars moons online, and began tightening down test precision; Hyperion is misbehaving.

--- libastro-3.7.2/marsmoon.c
+++ libastro-3.7.2/marsmoon.c
@@ -84,7 +84,7 @@ MoonData md[M_NMOONS])		/* return info */
 	md[2].mag = 12.9 + dmag;
 
 	/* get moon x,y,z from BDL if possible */
-	if (!dir || use_bdl (JD, dir, md) < 0) {
+	if (use_bdl (JD, dir, md) < 0) {
 	    int i;
 	    for (i = 1; i < M_NMOONS; i++)
 		md[i].x = md[i].y = md[i].z = 0.0;
@@ -115,43 +115,23 @@ double JD,		/* julian date */
 char dir[],		/* directory */
 MoonData md[M_NMOONS])	/* fill md[1..NM-1].x/y/z for each moon */
 {
-#define MRAU    .00002269       /* Mars radius, AU */
+#define MRAU 3389.9 /* Mars radius, km */
 	double x[M_NMOONS], y[M_NMOONS], z[M_NMOONS];
-	char buf[1024];
-	FILE *fp;
-	char *fn;
+	BDL_Dataset *dataset;
 	int i;
 
 	/* check ranges and appropriate data file */
 	if (JD < 2451179.50000)		/* Jan 1 1999 UTC */
 	    return (-1);
 	if (JD < 2455562.5)		/* Jan 1 2011 UTC */
-	    fn = "mars.9910";
+            dataset = & mars_9910;
 	else if (JD < 2459215.5)	/* Jan 1 2021 UTC */
-	    fn = "mars.1020";
+	    dataset = & mars_1020;
 	else
 	    return (-1);
 
-	/* open */
-	(void) sprintf (buf, "%s/%s", dir, fn);
-	fp = fopen (buf, "r");
-	if (!fp) {
-	    fprintf (stderr, "%s: %s\n", fn, strerror(errno));
-	    return (-1);
-	}
-
 	/* use it */
-	if ((i = read_bdl (fp, JD, x, y, z, buf)) < 0) {
-	    fprintf (stderr, "%s: %s\n", fn, buf);
-	    fclose (fp);
-	    return (-1);
-	}
-	if (i != M_NMOONS-1) {
-	    fprintf (stderr, "%s: BDL says %d moons, code expects %d", fn,
-								i, M_NMOONS-1);
-	    fclose (fp);
-	    return (-1);
-	}
+        do_bdl(dataset, JD, x, y, z);
 
 	/* copy into md[1..NM-1] with our scale and sign conventions */
 	for (i = 1; i < M_NMOONS; i++) {
@@ -161,7 +141,6 @@ MoonData md[M_NMOONS])	/* fill md[1..NM-1].x/y/z for each moon */
 	}
 
 	/* ok */
-	fclose (fp);
 	return (0);
 }
 
