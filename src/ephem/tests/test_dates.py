c3f8c32168e9203cf9b9ea9e3415ca65a9e45f57
Brandon Craig Rhodes <brandon@rhodesmill.org>
2009-06-24 01:03:54 +0000

    The Python functions that I use to convert dates into the user's local time zone in the convenience function ephem.localtime() turn out to be sensitive to the underlying OS's time_t size, so I have added some tests to test_dates.py that allow this result to be experimented with.

--- src/ephem/tests/test_dates.py
+++ src/ephem/tests/test_dates.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
-from ephem_test import *
+import unittest, time
+from datetime import datetime
+
+from ephem import Date, localtime
 
 # Determine whether dates behave reasonably.
 
 class date_suite(unittest.TestCase):
     def setUp(self):
-        self.date = date('2004/09/04 00:17:15.8')
+        self.date = Date('2004/09/04 00:17:15.8')
 
     def test_date_constructor(self):
 
         def construct_and_compare(args1, args2):
-            d1, d2 = date(*args1), date(*args2)
+            d1, d2 = Date(*args1), Date(*args2)
             self.assert_(-1e-15 < (d1 / d2 - 1) < 1e-15,
                          'dates not equal:\n %r = date%r\n %r = date%r'
                          % (d1.tuple(), args1, d2.tuple(), args2))
@@ -51,6 +54,21 @@ class date_suite(unittest.TestCase):
     def test_date_tuple_value(self):
         self.assertEqual(self.date.tuple(),
                          (2004, 9, 4, 0, 17, 15.799999977461994))
+
+    def test_localtime_modern(self):
+        if time.timezone == 18000: # test only works in Eastern time zone
+            self.assertEqual(localtime(Date('2009/6/23 8:47')),
+                             datetime(2009, 6, 23, 4, 47, 0, 1))
+
+    # I am commenting this out for now because I am not sure that I can
+    # fix it without either writing an entirely new time module for
+    # PyEphem, or making PyEphem depend on another Python module - which
+    # would be its first-ever external dependency.
+
+    def OFF_test_localtime_premodern(self):
+        if time.timezone == 18000: # test only works in Eastern time zone
+            self.assertEqual(localtime(Date('1531/8/24 2:49')),
+                             datetime(1957, 10, 4, 15, 28, 34, 4))
 
 if __name__ == '__main__':
     unittest.main()
