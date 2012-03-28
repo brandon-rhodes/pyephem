00f4f2e7c4d4aac9450cbaaf671438a135c02c0e
Brandon Craig Rhodes <brandon@rhodesmill.org>
2012-03-27 22:44:46 -0400

    Test fix from c6d8ca816106 to manually set time zone for documentation tests.

--- src/ephem/tests/test_rst.py
+++ src/ephem/tests/test_rst.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-import doctest, unittest
-from glob import glob
+import doctest
+import unittest
 import os.path
+import time
+from glob import glob
 
 def load_tests(loader, tests, pattern):
+
+    # Force time zone to EST/EDT to make localtime tests work.
+    os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+    time.tzset()
+
     return unittest.TestSuite([
             doctest.DocFileSuite('../doc/%s' % os.path.basename(path))
             for path in glob(os.path.dirname(__file__) + '/../doc/*.rst')
