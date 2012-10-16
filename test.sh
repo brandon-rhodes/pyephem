c1a7d31873604f47407cf5cb9b98c522cfaf6841
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-16 09:33:20 -0400

    Add bare test-module import check to "test.sh"

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 export PYTHONPATH=~/ephem4/src:~/sgp4:~/jplephem:$PYTHONPATH
+
+# Since "unittest" is bad at diagnosing import errors, we try bare
+# importation of the test module before setting unittest loose on it.
+
+python3 -c 'import ephem.tests.test_vs_novas' &&
+python2 -c 'import ephem.tests.test_vs_novas' &&
 python3 -m unittest ephem.tests.test_vs_novas &&
 python2 -m unittest ephem.tests.test_vs_novas
