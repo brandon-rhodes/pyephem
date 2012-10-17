fbb86f32146df378d612083da88816b31600e458
Brandon Rhodes <brandon@rhodesmill.org>
2012-10-17 13:13:42 -0400

    Make "test.sh" build two virtualenv's

--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-export PYTHONPATH=~/ephem4/src:~/sgp4:~/jplephem:$PYTHONPATH
+set -e
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+if [ ! -d venv2 ] ;then
+    python2 ~/usr/src/virtualenv/virtualenv.py --system-site-packages venv2
+    python3 ~/usr/src/virtualenv/virtualenv.py --system-site-packages venv3
+
+    venv2/bin/pip install novas_de405
+    venv3/bin/pip install novas_de405
+fi
+
+export PATH=$PWD/venv2/bin:$PWD/venv3/bin:$PATH
+export PYTHONPATH=~/ephem4/src:~/sgp4:~/jplephem:~/novas/build/lib.linux-i686-2.7:$PYTHONPATH
 
 # Since "unittest" is bad at diagnosing import errors, we try bare
 # importation of the test module before setting unittest loose on it.
