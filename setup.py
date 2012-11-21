1b495ef3ddec06c9ad646ecb9f4942d85cf740c4
Brandon Rhodes <brandon@rhodesmill.org>
2012-11-21 05:06:12 -0500

    Reorganize so that tox can run our tests
    This separates out the new PyEphem code, which now goes in a top-level
    "ephem" directory, from the old still-to-be-migrated (or abandoned) code
    in the old "src" directory.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 # Read the current version from ephem/__init__.py itself.
 
-path = os.path.join(os.path.dirname(__file__), 'src', 'ephem', '__init__.py')
+path = os.path.join(os.path.dirname(__file__), 'ephem', '__init__.py')
 for line in open(path):
     if line.startswith('__version__'):
         __version__ = eval(line.split(None, 2)[2]) # skip '__version__', '='
@@ -11,25 +11,25 @@ for line in open(path):
 def read(*filenames):
     return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
 
-setup(name = 'pyephem',
-      version = __version__,
-      description = 'Scientific-grade astronomy routines',
-      long_description = read('README'),
-      license = 'LGPL',
-      author = 'Brandon Craig Rhodes',
-      author_email = 'brandon@rhodesmill.org',
-      url = 'http://rhodesmill.org/pyephem/',
-      classifiers = [
+setup(
+    name='pyephem',
+    version=__version__,
+    description='Scientific-grade astronomy routines',
+    long_description=read('README'),
+    license='MIT',
+    author='Brandon Rhodes',
+    author_email='brandon@rhodesmill.org',
+    url='http://rhodesmill.org/pyephem/',
+    classifiers=[
         'Development Status :: 6 - Mature',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved ::'
-        ' GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Astronomy',
         ],
-      packages = [ 'ephem', 'ephem.tests' ],
-      package_dir = { '': 'src' },
-      package_data = { 'ephem': ['doc/*.rst',
-                                 'tests/jpl/*.txt',
-                                 'tests/usno/*.txt',
-                                 ],},
-      )
+    packages=[ 'ephem', 'ephem.tests' ],
+    package_data={ 'ephem': ['doc/*.rst',
+                             'tests/jpl/*.txt',
+                             'tests/usno/*.txt',
+                             ],},
+    install_requires=['jplephem', 'numpy', 'sgp4'],
+    )
