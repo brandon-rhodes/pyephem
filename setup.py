40572e34cbe83d9618966e600bdc0e0d880d07bf
Brandon Rhodes <brandon@rhodesmill.org>
2014-05-21 11:19:46 -0400

    Add Python 3 compatibility to setup.py classifiers

--- setup.py
+++ setup.py
@@ -19,12 +19,20 @@ libastro_data = glob('extensions/data/*.c')
 def read(*filenames):
     return open(os.path.join(os.path.dirname(__file__), *filenames)).read()
 
+extensions = [
+    Extension('ephem._libastro',
+              ['extensions/_libastro.c', 'extensions/dtoa.c']
+              + libastro_files + libastro_data,
+              include_dirs=['libastro-' + libastro_version],
+              ),
+    ]
+
 setup(name = 'pyephem',
       version = __version__,
-      description = 'Scientific-grade astronomy routines',
+      description = 'Compute positions of the planets and stars',
       long_description = read('README.rst'),
       license = 'LGPL',
-      author = 'Brandon Craig Rhodes',
+      author = 'Brandon Rhodes',
       author_email = 'brandon@rhodesmill.org',
       url = 'http://rhodesmill.org/pyephem/',
       classifiers = [
@@ -33,18 +41,18 @@ setup(name = 'pyephem',
         'License :: OSI Approved ::'
         ' GNU Library or Lesser General Public License (LGPL)',
         'Topic :: Scientific/Engineering :: Astronomy',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         ],
       packages = [ 'ephem', 'ephem.tests' ],
       package_data = { 'ephem': ['doc/*.rst',
                                  'tests/jpl/*.txt',
                                  'tests/usno/*.txt',
                                  ],},
-      ext_modules = [
-    Extension('ephem._libastro',
-              ['extensions/_libastro.c', 'extensions/dtoa.c']
-              + libastro_files + libastro_data,
-              include_dirs=['libastro-' + libastro_version],
-              )],
+      ext_modules = extensions,
       )
