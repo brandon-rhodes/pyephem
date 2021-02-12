1abfa0c5220b37c0e40e3585f31ec2b5231984a9
Lutz Mändle <lmaendle@gmx.net>
2021-02-12 11:33:25 -0500

    Add HTTPS support and fix Minor Planet Center URLs
    From https://www.clearskyinstitute.com/xephem/contrib/contrib.html which
    says it was contributed in January 2018.

--- GUI/xephem/auxil/mpcorb2edb.pl
+++ GUI/xephem/auxil/mpcorb2edb.pl
@@ -78,7 +78,7 @@ EOF
 # setup cutoff mag
 my $dimmag = 13;			# dimmest mag to be saved in "bright" file
 # set site and file in case of -f
-my $MPCSITE = "http://www.minorplanetcenter.net";
+my $MPCSITE = "https://www.minorplanetcenter.net";
 my $MPCFTPDIR = "/iau/MPCORB";
 my $MPCFILE = "MPCORB.DAT";
 my $MPCZIPFILE = "MPCORB.DAT.gz";
--- GUI/xephem/net.h
+++ GUI/xephem/net.h
@@ -20,6 +20,13 @@
 #include <sys/select.h>
 #endif
 
+#include <openssl/ssl.h>
+
+typedef struct {
+	int fd;		//file desciptor for the underlying connection socket
+	SSL *ssl;	//ssl connection for use with SSL_read( )and SSL_write()
+} XE_SSL_FD;
+
 /* support functions */
 
 extern int httpGET (char *host, char *GETcmd, char msg[]);
@@ -29,7 +36,10 @@ extern int readbytes (int fd, unsigned char buf[], int n);
 extern int recvline (int fd, char buf[], int max);
 extern int recvlineb (int sock, char *buf, int size);
 extern int sendbytes (int fd, unsigned char buf[], int n);
-
-
+extern int httpsGET (char *host, char *GETcmd, char msg[], XE_SSL_FD *ssl_fd);
+extern int ssl_recvbytes (XE_SSL_FD *ssl_fd, unsigned char buf[], int n);
+extern int ssl_readbytes (XE_SSL_FD *ssl_fd, unsigned char buf[], int n);
+extern int ssl_recvline (XE_SSL_FD *ssl_fd, char buf[], int max);
+extern int ssl_recvlineb (XE_SSL_FD *ssl_fd, char *buf, int size);
 
 #endif /* _NET_H */
