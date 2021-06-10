3e6626529b8920d5841a1f06537fd98d670e4d50
Brandon Rhodes <brandon@rhodesmill.org>
2021-06-10 16:57:27 -0400

    Declare version 4.0.0

--- ephem/__init__.py
+++ ephem/__init__.py
@@ -9,7 +9,7 @@ from datetime import tzinfo as _tzinfo
 from math import pi
 from time import localtime as _localtime
 
-__version__ = '3.7.7.1'
+__version__ = '4.0.0'
 
 twopi = pi * 2.
 halfpi = pi / 2.
