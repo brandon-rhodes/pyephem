26f12ea08a348bc744ebde72f3b173e0dcfb3c54
Serge Montagnac <obs-psr@orange.fr>
2021-07-21 12:16:17 +0200

    Add “saturne.2040” ephemeris and code (#21)
    The IMCCE/BDL 2020-2040 coefficients data file for Saturn and moons.

--- libastro/satmoon.c
+++ libastro/satmoon.c
@@ -143,6 +143,8 @@ MoonData md[S_NMOONS])	/* fill md[1..NM-1].x/y/z for each moon */
 	    fn = "saturne.9910";
 	else if (JD < 2459215.5)	/* Jan 1 2021 UTC */
 	    fn = "saturne.1020";
+	else if (JD < 2466520.5)	/* Jan 1 2041 UTC */
+	    fn = "saturne.2040";
 	else
 	    return (-1);
 
